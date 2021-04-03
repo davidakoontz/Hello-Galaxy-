cd459f531b37752742792e6f0e3f0de98eb3ecc6
davidakoontz <david@koontz.name>
2021-04-03 17:17:31 -0500

    Initial Commit

---
+++ Hello Galaxy?/Hello_Galaxy_App.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_Galaxy_App.swift
+//  Hello Galaxy?
+//
+//  Created by David on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Hello_Galaxy_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
